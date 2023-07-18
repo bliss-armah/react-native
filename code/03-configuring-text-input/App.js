c7c41f9a6dee9ef7a564c14b7723a3931e6fd5c1
Bliss  Armah-Nwanwah <bliss.armah-nwanwah@amalitech.com>
2023-07-18 16:09:39 +0000

    starter files

--- code/03-configuring-text-input/App.js
+++
@@ -1,9 +0,0 @@
-import { StyleSheet } from 'react-native';
-
-import StartGameScreen from './screens/StartGameScreen';
-
-export default function App() {
-  return <StartGameScreen />;
-}
-
-const styles = StyleSheet.create({});
--- extra-files/logic/random.js
+++
@@ -1,9 +0,0 @@
-function generateRandomBetween(min, max, exclude) {
-  const rndNum = Math.floor(Math.random() * (max - min)) + min;
-
-  if (rndNum === exclude) {
-    return generateRandomBetween(min, max, exclude);
-  } else {
-    return rndNum;
-  }
-}
