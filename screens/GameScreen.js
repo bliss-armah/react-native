50b6ca7fb3b7008ebf302eb8498cc0faaa90bd5e
Bliss  Armah-Nwanwah <bliss.armah-nwanwah@amalitech.com>
2023-07-21 14:01:01 +0000

    gamsescreen guess number implemented

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,34 +1,50 @@
-import { Text, View, StyleSheet } from "react-native"
-import Title from "../components/Title"
-import { Colors } from "../constants/colors"
+import { Text, View, StyleSheet } from "react-native";
+import Title from "../components/ui/Title";
+import { Colors } from "../constants/colors";
+import { useState } from "react";
+import NumberContainer from "../components/game/NumberContainer";
 
-const GameScreen = () => {
-  return( <View style={styles.screen}>
-    <Title>Opponent's Guess</Title>
-    {/* Guess */}
-    <View>
-      <Text>Higher or lower?</Text>
-      {/* + - */}
-    </View>
-    {/* <View>LOG ROUNDS</View> */}
-  </View>
-  )
+function generateRandomBetween(min, max, exclude) {
+  const rndNum = Math.floor(Math.random() * (max - min)) + min;
+
+  if (rndNum === exclude) {
+    return generateRandomBetween(min, max, exclude);
+  } else {
+    return rndNum;
+  }
 }
 
-export default GameScreen
+const GameScreen = ({userNumber}) => {
+  const initialGuess = generateRandomBetween(1,100,userNumber)
+  const [currentGuess, setCurrentGuess] = useState(initialGuess);
+
+  return (
+    <View style={styles.screen}>
+      <Title>Opponent's Guess</Title>
+      <NumberContainer>{currentGuess}</NumberContainer>
+      <View>
+        <Text>Higher or lower?</Text>
+        {/* + - */}
+      </View>
+      {/* <View>LOG ROUNDS</View> */}
+    </View>
+  );
+};
+
+export default GameScreen;
 
 const styles = StyleSheet.create({
-  screen:{
-    flex:1,
-    padding:24
+  screen: {
+    flex: 1,
+    padding: 24,
   },
-  title:{
-    fontSize:24,
-    fontWeight:'bold',
-    color:Colors.accent500,
-    textAlign:'center',
-    borderWidth:2,
-    borderColor:Colors.accent500,
-    padding:12,
-  }
-})
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: Colors.accent500,
+    textAlign: "center",
+    borderWidth: 2,
+    borderColor: Colors.accent500,
+    padding: 12,
+  },
+});
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,7 +1,7 @@
 import { View, TextInput, StyleSheet, Alert } from "react-native";
-import PrimaryButton from "../components/PrimaryButton";
 import { useState } from "react";
 import { Colors } from "../constants/colors";
+import PrimaryButton from "../components/ui/PrimaryButton";
 
 const StartGameScreen = ({onPickNumber}) => {
   const [enteredNumber, setEnteredNumber] = useState("");
